a7812e09940ef2f0b3a1651cdc510e22f9d03d9c
angelk <74609669+angelinakoloska@users.noreply.github.com>
2024-03-14 10:44:27 +0100

    Update file

--- routes/index.js
+++ routes/index.js
@@ -1,6 +1,7 @@
 var express = require('express');
 var router = express.Router();
-
+require('dotenv').config()
+var jwt = require('jsonwebtoken')
 
 const users = [];
 let id = 1;
@@ -31,11 +32,24 @@ router.post("/login", async (req, res, next) => {
     const error = Error("Wrong details please check at once");
     return next(error);
   }
+  let token;
+  try {
+    token = jwt.sign(
+      { userId: existingUser.id, email: existingUser.email },
+      process.env.TOKEN_SECRET,
+      { expiresIn: "1h" }
+    );
+  } catch (err) {
+    console.log(err);
+    const error = new Error("Error! Something went wrong.");
+    return next(error);
+  }
   res.status(200).json({
     success: true,
     data: {
       userId: existingUser.id,
       email: existingUser.email,
+      token: token
     },
   });
 });
@@ -50,11 +64,24 @@ router.post("/signup", async (req, res, next) => {
   };
   users.push(newUser);
   id++;
+  let token;
+  try {
+    token = jwt.sign(
+      { userId: newUser.id, email: newUser.email },
+      process.env.TOKEN_SECRET,
+      { expiresIn: "1h" }
+    );
+  } catch (err) {
+    console.log(err);
+    const error = new Error("Error! Something went wrong.");
+    return next(error);
+  }
   res.status(201).json({
     success: true,
     data: {
       userId: newUser.id,
       email: newUser.email,
+      token: token
     },
   });
 });
