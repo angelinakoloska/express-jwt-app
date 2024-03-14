c16c9ecab0979b3ce2a9e151f5468a15b32c7a6d
angelk <74609669+angelinakoloska@users.noreply.github.com>
2024-03-14 10:47:55 +0100

    Add database to app.js

--- app.js
+++ app.js
@@ -7,6 +7,9 @@ var logger = require('morgan');
 var indexRouter = require('./routes/index');
 var usersRouter = require('./routes/users');
 
+var db = require("./models");
+db.se
+
 var app = express();
 
 // view engine setup
