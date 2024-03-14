3073aa0da3e2994ef5867fd20150e19ebe388937
angelk <74609669+angelinakoloska@users.noreply.github.com>
2024-03-14 10:56:16 +0100

    Modify users.js

--- routes/users.js
+++ routes/users.js
@@ -1,21 +1,24 @@
 var express = require('express');
 var router = express.Router();
-var jwt = require('jsonwebtoken')
+var jwt = require('jsonwebtoken');
+var db = require("../models");
+var UserService = require("../services/UserService");
+var userService = new UserService(db);
 
 /* GET users listing. */
-router.get('/', function(req, res, next) {
-  const token = req.headers.authorization.split(' ')[1];
-  if(!token)
-  {
-      res.status(200).json({success:false, message: "Error! Token was not provided."});
+router.get('/', async function(req, res, next) {
+  const token = req.headers.authorization?.split(' ')[1];
+  if(!token) {
+    res.status(200).json({success:false, message: "Error! Token was not provided."});
   }
-  const decodedToken = jwt.verify(token, process.env.TOKEN_SECRET );
+  const decodedToken = jwt.verify(token, process.env.TOKEN_SECRET);
+  const users = await userService.getAll();
   res.status(200).json({
     success:true,
     data:{
       userId:decodedToken.userId,
       email:decodedToken.email,
-      users: ['user1', 'user2', 'user3']
+      users: users.map(user=>user.Email)
     }
   });
 });
