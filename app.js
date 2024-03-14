429d079cb9260df6ee1fe1d07e5278df48ab4e58
angelk <74609669+angelinakoloska@users.noreply.github.com>
2024-03-14 10:48:15 +0100

    update app.js

--- app.js
+++ app.js
@@ -8,7 +8,7 @@ var indexRouter = require('./routes/index');
 var usersRouter = require('./routes/users');
 
 var db = require("./models");
-db.se
+db.sequelize.sync({ force: false })
 
 var app = express();
 
